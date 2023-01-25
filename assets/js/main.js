5fb3562b77ae6a8b1a93f6d60fb69a5791845e7e
RashidaAvshar <rashidaavshar@gmail.com>
2023-01-25 15:30:55 +0400

    to do list

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+document.querySelector("#btn_add").addEventListener("click", ()=>{
+    const inputTask = document.querySelector("#taskinput").value;
+    if(inputTask.trim()==""){
+        alert("xanaya deyer elave edin");
+    }else{
+        document.querySelector("#task_list").innerHTML +=`<div class="task_item">
+            <span>${inputTask}</span>
+            <button id="delete">X</button>
+        </div>`;
+        document.querySelector("#taskinput").value="";
+        const deleteTask = document.querySelectorAll("#delete");
+        for(let i=0; i< deleteTask.length; i++){
+            deleteTask[i].onclick = function(){
+                deleteTask[i].parentNode.remove();
+            }
+        }
+    }
+})
